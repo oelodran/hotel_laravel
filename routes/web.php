2bc8060e58b08f3b3ca257493221468aebedfa81
Leonardo Ramljak <leonardo.ramljak@tel.net.ba>
2020-12-11 09:49:57 +0100

    make authentication, migration, seeding, ShowRoomsController

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ShowRoomsController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +17,13 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', function () {return 'Goodbye';});
+
+Route::get('/rooms', ShowRoomsController::class);
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth'])->name('dashboard');
+
+require __DIR__.'/auth.php';
